f74a9919f6bddd04dfeefce546360632c382fc90
yarmail <yarmail@yandex.ru>
2023-03-27 17:47:28 +0300

    добавляем добавляем клиент

---
+++ eureka_client/src/main/java/com/example/eureka_client/EurekaClientApplication.java
@@ -0,0 +1,23 @@
+package com.example.eureka_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class EurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+
+}
+
+/* Примечание
+Есть ещё @EnableDiscoveryClient используется когда у вас сервер не Eureka
+а какой-то другой
+ */
+
+
