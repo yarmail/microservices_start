85d4aa91d96c823d4cfe0fa40f46d97bc17c3c13
yarmail <yarmail@yandex.ru>
2023-03-30 16:20:32 +0300

    добавляем ещё микросервисы

--- eureka_client/src/main/java/com/example/eureka_client/controller/TestController.java
+++ eureka_client/src/main/java/com/example/eureka_client/controller/TestController.java
@@ -1,4 +1,5 @@
 package com.example.eureka_client.controller;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/main")
 public class TestController {
 
+    //получаем id из настроек
+    @Value("${eureka.instance.instance-id}")
+    private String id;
+
+    //возврат id покажет какой именно сервис отозвался
     @GetMapping("/test")
     public String test() {
-        return "test";
+        return id;
     }
 }
