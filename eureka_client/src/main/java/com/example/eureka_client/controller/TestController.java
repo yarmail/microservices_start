c38bc6f05139a8ee0fd6899c7a98ab3e6b9c87f8
yarmail <yarmail@yandex.ru>
2023-03-30 16:33:53 +0300

    load balancer (LB)

--- eureka_client/src/main/java/com/example/eureka_client/controller/TestController.java
+++ eureka_client/src/main/java/com/example/eureka_client/controller/TestController.java
@@ -1,5 +1,4 @@
 package com.example.eureka_client.controller;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,13 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/main")
 public class TestController {
 
-    //получаем id из настроек
-    @Value("${eureka.instance.instance-id}")
-    private String id;
-
-    //возврат id покажет какой именно сервис отозвался
     @GetMapping("/test")
     public String test() {
-        return id;
+        return "test";
     }
 }
